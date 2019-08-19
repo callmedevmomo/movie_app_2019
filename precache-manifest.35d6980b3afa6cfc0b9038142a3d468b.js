63909092edcc6b4d5f6d4e433548b4cfb1cb896d
momo <callmedevmomo@gmail.com>
2019-08-19 14:01:52 +0900

    Updates

--- precache-manifest.35d6980b3afa6cfc0b9038142a3d468b.js
+++ precache-manifest.35d6980b3afa6cfc0b9038142a3d468b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ec415ee5b673388ab1461371ce72ebb",
+    "revision": "21bbc002611a147164fa4537cf032472",
     "url": "/movie_app_2019/index.html"
   },
   {
-    "revision": "160247d23cb577bb3a58",
-    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+    "revision": "33a0ebb06abbe9ff3886",
+    "url": "/movie_app_2019/static/css/main.3a7b1607.chunk.css"
   },
   {
     "revision": "0412129e6fe003322cd1",
     "url": "/movie_app_2019/static/js/2.af5468dd.chunk.js"
   },
   {
-    "revision": "160247d23cb577bb3a58",
-    "url": "/movie_app_2019/static/js/main.b6118a15.chunk.js"
+    "revision": "33a0ebb06abbe9ff3886",
+    "url": "/movie_app_2019/static/js/main.73a29802.chunk.js"
   },
   {
     "revision": "ba3076a0c8d176265049",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2019/precache-manifest.7cb115aa916d3c5b213ba68baa27aa2d.js"
+  "/movie_app_2019/precache-manifest.35d6980b3afa6cfc0b9038142a3d468b.js"
 );
 
 self.addEventListener('message', (event) => {
